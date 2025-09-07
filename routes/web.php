cc0e708e3187e7599404904f16a72ee80cad1804
Haza Gomez <elhaza@gmail.com>
2025-09-06 22:08:14 -0700

    fix(view): rename Contact view reference to lowercase (Linux compatibility)

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('contacto', function () {return view('Contact');})->name('contact');
+Route::get('contacto', function () {return view('contact');})->name('contact');
 Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
